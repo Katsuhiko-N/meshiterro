6968230cf57797a3708e8ab34211998997218bde
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-07-22 08:45:56 +0000

    [Fix]コメント一覧・投稿機能バグ修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,23 +2,17 @@ class PostCommentsController < ApplicationController
     # コメント投稿機能
     def create
         post_image = PostImage.find(params[:post_image_id])
-        
-        # 以下の文を省略
         # ログイン中ユーザーに限定したコメント入れる用のモデル作成
         # comment = PostComment.new(post_comment_params)
+        # コメントしたユーザーID＝ログイン中のID
         # comment.user_id = current_user.id
+        # これらを省略すると下記の通り
         comment = current_user.post_comments.new(post_comment_params)
+        # コメントされた投稿画像ID＝PostImageモデルのID
+        comment.post_image_id = post_image.id
         comment.save
-        redirect_to post_image_path
+        redirect_to post_image_path(post_image)
     end
-    
-    # 投稿ページ
-    def show
-        @post_image = PostImage.find(params[:id])
-        @post_comment = PostComment.new
-    end
-        
-    
     
     
     private
