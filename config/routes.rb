869102ecf0e9650b43ac61f5c6a708d4de9bdbaa
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-07-30 08:10:14 +0000

    [Fix]ページネーションにかかわるアクション修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
+
   devise_for :users
   root to: "homes#top"
   get 'users/sign_up' => 'devise/sessions#new'
